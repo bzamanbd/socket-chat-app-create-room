a67a02a81789fdddaad249b38a39d59aa101bc7d
bzamanbd <riponzamanbd@gmail.com>
2024-04-27 14:14:29 +0600

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express') 
+const app = express() 
+const http = require('http') 
+const server = http.createServer(app) 
+const { Server } = require('socket.io') 
+const io = new Server(server) 
+const PORT = 8000 
+server.listen(PORT, () => { 
+    console.log(`server is running on http://localhost:${PORT}`);
+})
+app.get('/', (req,res) => { 
+    res.sendFile(__dirname+"/index.html")
+})
+
+io.on('connection', (socket) => { 
+    socket.join("room1") 
+    io.sockets.in('room1').emit('room1Chat', 'we are chatting in room1')
+})
